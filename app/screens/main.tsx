596ec5f0867ada9af80756411a799a464ee5566a
Ruan Naude <ruan@doshex.com>
2025-01-21 06:58:02 +0200

    Testing different versions of packages

--- app/screens/main.tsx
+++ app/screens/main.tsx
@@ -1,11 +1,10 @@
-import { Select, SelectItem } from "@nextui-org/react";
+import { Select, SelectItem } from "@heroui/react";
 
 const MONTHS = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
 
 export function MainScreen() {
   return (
     <div className="">
-      {/* @ts-ignore */}
       <Select label="Select a month of the year...">
         {MONTHS.map((month) => (
           <SelectItem key={month} value={month}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
-const { nextui } = require("@nextui-org/react");
+const { heroui } = require("@heroui/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{html,ts,tsx}", //
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [
-    nextui(), //
+    heroui(), //
   ],
 };
